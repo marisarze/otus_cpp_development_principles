e88d8498d8760ebb3f74153d388977516f9faf9d
marisarze <flypetrov@yandex.ru>
2022-10-01 12:23:05 +0300

    tab content change

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ int main(int argc, char const *argv[])
         editor.open_file("Some unexpected path for loading");
         editor.current_tab->add_figure(std::make_shared<Circle>());
         editor.current_tab->content[0]->change();
-        editor.current_tab->add_figure(std::make_shared<Square>());
-        editor.current_tab->content[0]->change();
-        editor.current_tab->save("Another unexpected path for saving");
-        editor.create_new_tab();
-        editor.current_tab->add_figure(std::make_shared<Rectangle>());
-        editor.current_tab->content[0]->change();
-        editor.current_tab->add_figure(std::make_shared<Triangle>());
-        editor.current_tab->content[0]->change();
-        editor.current_tab->remove_figure(editor.current_tab->content[0]);
-        editor.current_tab->save("Secret path for saving");
-        editor.close_tab(editor.open_tabs[0]);
-        editor.close_tab(editor.open_tabs[0]);
-        editor.stop();
+        // editor.current_tab->add_figure(std::make_shared<Square>());
+        // editor.current_tab->content[0]->change();
+        // editor.current_tab->save("Another unexpected path for saving");
+        // editor.create_new_tab();
+        // editor.current_tab->add_figure(std::make_shared<Rectangle>());
+        // editor.current_tab->content[0]->change();
+        // editor.current_tab->add_figure(std::make_shared<Triangle>());
+        // editor.current_tab->content[0]->change();
+        // editor.current_tab->remove_figure(editor.current_tab->content[0]);
+        // editor.current_tab->save("Secret path for saving");
+        // editor.close_tab(editor.open_tabs[0]);
+        // editor.close_tab(editor.open_tabs[0]);
+        // editor.stop();
     }
     catch(const std::exception &e)
     {
