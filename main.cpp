6ab2cd1b8403cc12be275c6694f2c788191d0b7c
marisarze <flypetrov@yandex.ru>
2022-10-01 12:26:23 +0300

    create new tab

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char const *argv[])
         editor.current_tab->content[0]->change();
         editor.current_tab->add_figure(std::make_shared<Square>());
         editor.current_tab->content[0]->change();
-        // editor.current_tab->save("Another unexpected path for saving");
-        // editor.create_new_tab();
+        editor.current_tab->save("Another unexpected path for saving");
+        editor.create_new_tab();
         // editor.current_tab->add_figure(std::make_shared<Rectangle>());
         // editor.current_tab->content[0]->change();
         // editor.current_tab->add_figure(std::make_shared<Triangle>());
