6a36c6fe4c53fbdc234c297aa985cd8f31190695
marisarze <flypetrov@yandex.ru>
2022-10-01 21:39:00 +0300

    create doxygen

--- editor.cpp
+++ editor.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file editor.cpp
+ * @author marisarze (you@domain.com)
+ * @brief 
+ * @version MY_PROJECT_VERSION
+ * @date BUILD_DATE
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <iostream>
 #include <vector>
 #include "editor.h"
@@ -10,43 +21,94 @@
 
 #define UNUSED(variable) (void)variable
 
+
+/**
+ * @brief Utility function for printing messages using <fmt> library plus new line character.
+ * Usage: println("Some sentence for formatting {}", "XXX") results to "Some sentence for formatting XXX"
+ *  
+ * @tparam Args types of parameters for formatting target message
+ * @param format target message for printing
+ * @param args formatting variables
+ */
 template <typename... Args>
 void println(std::string format, const Args&... args) {
   fmt::print(format, args...);
   std::putc('\n', stdout);
 }
 
+/**
+ * @brief implementation of the method for changing the graphic primitive "Square"
+ * 
+ */
 void Square::change() {
     println("SQUARE is changed");
 }
 
+/**
+ * @brief implementation of the method for changing the graphic primitive "Circle"
+ * 
+ */
 void Circle::change() {
     println("CIRCLE is changed");
 }
 
+/**
+ * @brief implementation of the method for changing the graphic primitive "Triangle"
+ * 
+ */
 void Triangle::change() {
     println("TRIANGLE is changed");
 }
 
+/**
+ * @brief implementation of the method for changing the graphic primitive "Rectangle"
+ * 
+ */
 void Rectangle::change() {
     println("RECTANGLE is changed");
 }
 
+/**
+ * @brief Construct a new Tab:: Tab object
+ * 
+ */
 Tab::Tab(){}
 
+
+/**
+ * @brief Construct a new Tab:: Tab object from path
+ * 
+ * @param path path for loading tab (new document)
+ */
 Tab::Tab(std::string path){
     UNUSED(path);
 }
 
+/**
+ * @brief saving Tab to document 
+ * 
+ * @param path path for saving the tab
+ */
 void Tab::save(std::string path){
     println("Tab saved to \'{}\'", path);
 }
 
+/**
+ * @brief Method for adding figure to tab content
+ * 
+ * @param figure target figure for adding
+ */
 void Tab::add_figure(std::shared_ptr<Figure> figure){
     content.emplace_back(figure);
     println("New figure {} was added to tab {}", fmt::ptr(figure), fmt::ptr(this));
 }
 
+/**
+ * @brief Method for removing figure from tab content, if figure is not in content prints
+ *  corresponding message about nothing to remove.
+ * 
+ * @param figure target figure for remove
+ */
 void Tab::remove_figure(std::shared_ptr<Figure> figure){
     auto it = std::find(content.begin(), content.end(), figure);
     if (it != content.end()){
@@ -57,10 +119,19 @@ void Tab::remove_figure(std::shared_ptr<Figure> figure){
     }
 }
 
+/**
+ * @brief Construct a new Editor:: Editor object
+ * 
+ */
 Editor::Editor(){
     println("Editor created");
 }
 
+/**
+ * @brief loading new document to open tabs
+ * 
+ * @param path path for loading new tab
+ */
 void Editor::open_file(std::string path){
     auto new_tab = std::make_shared<Tab>(path); 
     open_tabs.emplace_back(new_tab);
@@ -68,6 +139,10 @@ void Editor::open_file(std::string path){
     change_focus_to(new_tab);
 }
 
+/**
+ * @brief creating new empty tab
+ * 
+ */
 void Editor::create_new_tab(){
     auto new_tab = std::make_shared<Tab>(); 
     open_tabs.emplace_back(new_tab);
@@ -75,6 +150,12 @@ void Editor::create_new_tab(){
     change_focus_to(new_tab);
 }
 
+/**
+ * @brief closing tab (removing it from open tabs). 
+ * If the tab does not exist among the open ones, then displays the appropriate message.
+ *  
+ * @param tab target tab for closing
+ */
 void Editor::close_tab(std::shared_ptr<Tab> tab){
     auto it = std::find(open_tabs.begin(), open_tabs.end(), tab);
     if (it != open_tabs.end()){
@@ -90,6 +171,11 @@ void Editor::close_tab(std::shared_ptr<Tab> tab){
     }
 }
 
+/**
+ * @brief changing focus to target tab through setting "current_tab" parameter value.
+ * if tab is not among open tabs, then corresponging warning will be prompt.
+ * @param tab Target tab to shift focus.
+ */
 void Editor::change_focus_to(std::shared_ptr<Tab> tab){
     auto it = std::find(open_tabs.begin(), open_tabs.end(), tab);
     if (it != open_tabs.end()){
@@ -100,10 +186,18 @@ void Editor::change_focus_to(std::shared_ptr<Tab> tab){
     }
 }
 
+/**
+ * @brief Starting of abstract editor
+ * 
+ */
 void Editor::start(){
     println("Starting the engine");
 }
 
+/**
+ * @brief Storpping of abstract editor
+ * 
+ */
 void Editor::stop(){
     println("Preparing for stopping the engine...Stopped");
 }
